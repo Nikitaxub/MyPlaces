050b94963a2e826ade022797e7dcb4c9aa56f0f9
xubuntus <nikita.dmd@gmail.com>
2021-05-20 20:50:48 +0300

    CustomViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by xubuntus on 20.05.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+}
